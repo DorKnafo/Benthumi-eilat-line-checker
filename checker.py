6b325bc9513e0647c538104222057e04d4a42e1b
Dor Knafo <dor@fire.glass>
2015-12-20 16:43:26 +0200

    Good news speech added for Mac OSX users

--- checker.py
+++ checker.py
@@ -1,6 +1,9 @@
 import requests,time
 from bs4 import BeautifulSoup
+import os
+
 lastline = 0
+
 while True:
     try:
         ans = requests.get("http://eilat.dorbass.com/torshow.php")
@@ -8,10 +11,12 @@ while True:
         h3 = soup.find_all("h3")[0]
         line = int(str(h3.contents[0]).strip())
         if line > lastline:
-            print('\a') #beep
+            if os.name == 'posix':
+                os.system('say -v "Good news" You are next in line')
+            else:
+                print('\a') #beep
             print "%s line: %d"%(time.strftime("%H:%M"),line)
         lastline = line
     except:
         print "Error checking server. Trying again in 30 seconds."
     time.sleep(30)
-
