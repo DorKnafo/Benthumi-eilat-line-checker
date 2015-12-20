8e2e271501733e6aee484d8f0dfbde7245072d94
Gil Azrielant <gilazr@gmail.com>
2015-12-20 13:10:20 +0200

    Added checker

---
+++ checker.py
@@ -0,0 +1,17 @@
+import requests,time
+from bs4 import BeautifulSoup
+lastline = 0
+while True:
+    try:
+        ans = requests.get("http://eilat.dorbass.com/torshow.php")
+        soup = BeautifulSoup(ans.content, 'html.parser')
+        h3 = soup.find_all("h3")[0]
+        line = int(str(h3.contents[0]).strip())
+        if line > lastline:
+            print('\a') #beep
+            print "%s line: %d"%(time.strftime("%H:%M"),line)
+        lastline = line
+    except:
+        print "Error checking server. Trying again in 30 seconds."
+    time.sleep(30)
+
